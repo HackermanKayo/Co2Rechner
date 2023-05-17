7b300a7da391d23a6c97e5c4d4f09e8433fece85
HackermanKayo <79974201+HackermanKayo@users.noreply.github.com>
2023-05-17 19:46:19 +0200

    Add files via upload

---
+++ Co2RechnerView/src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage startStage) {
+		try {
+			
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));			
+			Scene startScene = new Scene(root);		
+			startScene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+//			String css = this.getClass().getResource("style.css").toExternalForm();
+//			sceneStart.getStylesheets().add(css);
+			
+	
+			startStage.setTitle("CO2-Rechner");
+			startStage.setScene(startScene);
+			startStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
