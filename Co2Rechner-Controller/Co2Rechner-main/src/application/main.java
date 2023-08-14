9d094967619ab5c3c78ec39943bd8ecd4959ed8b
Mohamed Elslakawy <132443955+MohamedElslakawy@users.noreply.github.com>
2023-08-14 22:42:10 +0300

    Add files via upload
    Control

---
+++ Co2Rechner-Controller/Co2Rechner-main/src/application/main.java
@@ -0,0 +1,24 @@
+package application;
+
+
+public class main {
+
+	// private View view;
+	  //private Model model;
+
+	public main() {
+		
+	}
+	public static void main(String[] args) {
+		Controller controller = new Controller(null); 
+		
+		
+		//controller.run();
+		
+
+}
+	
+	
+		
+		
+}
