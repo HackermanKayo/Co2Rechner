232283eee50073fc02edc36eb79fba89a0b472ec
Mohamed Elslakawy <132443955+MohamedElslakawy@users.noreply.github.com>
2023-08-14 23:03:18 +0300

    Add files via upload

---
+++ Co2Rechner-Controller/Co2Rechner-main/src/application/Mobilität.java
@@ -0,0 +1,44 @@
+package application;
+enum Fahrzeugs {Kleinwage,MittelKlasse,OberKlasse,KfZ,Elektrofahrzeug,Motorrad,Roller,Elektroroller};
+enum Kraftstoff{Benzin,Diesel,Erdgas,Flüssiggas};
+enum verkehrsmittel {Eigene,Carsharing,Fernverkehr,SonstigerBahnfernverkehr,ZugNahverkehr,ÖPNV,Reisebus,Fahrrad,};
+public class Mobilität  {
+private String FahrzeugBezeichnung;
+private double Durchschnittsverbrauch;
+private int fahrzeugeAlter,fahrtenAnzahl;
+private long distanz;
+public static void main(String[] args) {
+	Fahrzeugs Fahrzeug = Fahrzeugs.Kleinwage;
+	
+	switch(Fahrzeug){
+	case Kleinwage:
+		System.out.println("Kleinwage");
+        break;
+    case MittelKlasse:
+    	System.out.println("Mittelklasse");
+        break;
+    case OberKlasse:	
+    	System.out.println("Oberklasse");
+        break;
+    case KfZ:
+    	System.out.println("KfZ");
+        break;
+    case Elektrofahrzeug:
+    	System.out.println("Elektrofahrzeug");
+        break;
+    case Motorrad:
+    	System.out.println("Motorrad");
+        break;
+    case Roller:
+    	System.out.println("Roller");
+        break;
+    case Elektroroller:
+    	System.out.println("Elektroroller");
+        break;
+        
+	
+	
+	}
+	
+}
+}
---
+++ Co2Rechner-Controller/Co2Rechner-main/src/application/Wohnen.java
@@ -0,0 +1,14 @@
+package application;
+enum hausTyp {Mehrfamilienhaus,Reihenhaus,Einfamilienhaus};
+
+public class Wohnen {
+public Wohnen() {
+		
+		// TODO Auto-generated constructor stub
+	}
+private String WohnenTyp,HeizungArt;
+private int BewohnerAnzahl,Baujahr,Strom;
+private String Stadt,Land;
+private boolean solaranlage,Mülltrennung;
+
+}
